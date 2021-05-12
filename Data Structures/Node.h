9dd279cb73011ec2eaab9aac2c32f253a56b67c1
Joseph Ameer <joujou_ameer@yahoo.com>
2021-05-13 00:00:44 +0200

    Added Queue.h and Node.h to Master

---
+++ Data Structures/Node.h
@@ -0,0 +1,39 @@
+#pragma once
+template <class T>
+class Node
+{
+private:
+	T Item;
+	Node<T>* Next;
+public:
+	Node()
+	{
+		Next = NULL;
+	}
+	Node(T nItem)
+	{
+		Item = nItem;
+		Next = NULL;
+	}
+	void setitem(T nItem)
+	{
+		Item = nItem;
+	}
+	T getitem()
+	{
+		return Item;
+	}
+	Node<T>* getNext()
+	{
+		return Next;
+	}
+	void setNext(Node<T>* NextNode)
+	{
+		Next = NextNode;
+	}
+
+
+
+
+};
+
--- Data Structures/Queue.h
+++ Data Structures/Queue.h
@@ -1,7 +1,93 @@
 #pragma once
+#include"Node.h"
+template<class T>
 class Queue
 {
+	Node<T>* Head;
+	Node<T>* Rear;
+public:
 
+	Queue()
+	{
+		Head = nullptr;
+		Rear = nullptr;
+	}
 
+	Queue(T item)
+	{
+		Node<T>* node = new Node<T>(item);
+		Head = node;
+		Rear = Head;
+	}
+
+	bool IsEmpty()
+	{
+		return Head = nullptr;
+	}
+
+	bool Enqueue(T item)
+	{
+		if (!Head)
+		{
+			Node<T>* node = new Node<T>(item);
+			Head = node;
+			Rear = Head;
+			return true;
+		}
+		else
+		{
+			Node<T>* node = new Node<T>(item);
+			Rear->setNext(node);
+			Rear = Rear->getNext();
+			return true;
+		}
+	}
+
+	bool dequeue(T& item)
+	{
+		if (IsEmpty())
+			return false;
+		item = Head->getitem();
+		Node<T>* temp = Head->getNext();
+		delete Head;
+		Head = temp;
+		return true;
+	}
+
+	/*bool Retrieve(T item)
+	{
+		Node<T>* L, * N;
+		L = Head;
+		N = L->getNext();
+		if (Head->getitem() == item)
+		{
+			delete Head;
+			Head = N;
+			return true;
+		}
+		Node<T>* temp;
+		while (N)
+		{
+			if (N->getitem() == item)
+			{
+				temp = N->getNext();
+				L->setNext(temp);
+				delete N;
+				return true;
+			}
+			else
+			{
+				L = L->getNext();
+				N = N->getNext();
+			}
+		}
+		return false;
+	}*/
+
+	~Queue()
+	{
+		T temp;
+		while (dequeue(T));
+	}
 };
 
